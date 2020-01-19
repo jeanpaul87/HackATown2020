c0180e070127fb72d52cd2b1caf59afc90dfb70a
jeanpaul87 <59045894+jeanpaul87@users.noreply.github.com>
2020-01-19 11:54:25 -0500

    Add files via upload

---
+++ Gps.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gps : MonoBehaviour
+{
+    public static Gps Instance { set; get; }
+
+    public static float lattitude;
+    public static float longtitude;
+
+    private void Start()
+    {
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+        StartCoroutine(StartLocationService());
+    }
+
+    private IEnumerator StartLocationService()
+    {
+        if (!Input.location.isEnabledByUser)
+        {
+            Debug.Log("User has not enabled GPS !");
+            yield break;
+        }
+        Input.location.Start();
+        int maxWait = 20;
+        while(Input.location.status == LocationServiceStatus.Initializing && maxWait > 0)
+        {
+            yield return new WaitForSeconds(1);
+            maxWait--;
+        }
+
+        if(maxWait <= 0) { Debug.Log("TIME OUT");
+            yield break;
+        }
+
+        if(Input.location.status == LocationServiceStatus.Failed)
+        {
+            Debug.Log("Unable to determin device location");
+            yield break;
+        }
+
+        lattitude = Input.location.lastData.latitude;
+        longtitude = Input.location.lastData.longitude;
+        
+        yield break;
+    }
+}
---
+++ takeScreenshot.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.Net;
+using System.Net.Mail;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+
+
+public class takeScreenshot : MonoBehaviour
+{
+    //  [SerializeField] GameObject blink;
+
+    public string youremail;
+    public string subject;
+ 
+    public string mailBodyy;
+    public Text errrrror;
+    public string pathh;
+
+    public GameObject pannel;
+
+
+    private void Start()
+    {
+        pannel.SetActive(true);
+    }
+    public void removeThePannelByClicking()
+    {
+        pannel.SetActive(false);
+    }
+    private void Update()
+    {
+
+        errrrror.text = "lattitude :" + Gps.lattitude.ToString()+ " , longtitude: " + Gps.longtitude.ToString() ; 
+
+    }
+
+
+    public void TakeAShot()
+    {
+        StartCoroutine("CaptureIt");
+        StartCoroutine(sendmail());
+        //Sennd Email
+        // public Text coor
+        //coor.text = "Lat :"GPS.Instance.latitude.ToString();
+        //              Longitude...
+        
+
+    }
+    IEnumerator CaptureIt()
+    {
+        
+        string timeStamp = System.DateTime.Now.ToString("dd-MM-yyyy-HH-mm-ss");
+        string fileName = "ScreenShot" + timeStamp + ".png";
+        string pathToSave = fileName;
+        pathh = pathToSave ;
+        ScreenCapture.CaptureScreenshot(pathToSave);
+        yield return new WaitForEndOfFrame();
+    }
+    public IEnumerator sendmail()
+    {
+        yield return new WaitForSeconds(0.0f);
+        MailMessage mail = new MailMessage();
+        mail.From = new MailAddress(youremail);
+        mail.To.Add("jp.khoueiry@gmail.com");
+        mail.Subject = subject;
+        mail.Body = mailBodyy + "https://www.google.com/maps/search/?api=1&query="+Gps.lattitude+","+Gps.longtitude;
+
+        Attachment attachment = null;
+        attachment = new Attachment(pathh);
+        mail.Attachments.Add(attachment);
+
+
+        SmtpClient smtpServer = new SmtpClient("smtp.gmail.com");
+        smtpServer.Port = 587;
+        smtpServer.Credentials = new System.Net.NetworkCredential(youremail, "!@#456qwe") as ICredentialsByHost;
+        smtpServer.EnableSsl = true;
+        ServicePointManager.ServerCertificateValidationCallback =
+            delegate (object s, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslPolicyErrors) { return true; };
+   
+        smtpServer.Send(mail);
+        errrrror.text = "The Mail was sent successessfully";
+    }
+}
